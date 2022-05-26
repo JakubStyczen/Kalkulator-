cebae13fca8e1c7e7084780966744eea88470091
Jakub Styczzen <styczen37@gmail.com>
2022-05-26 17:34:35 +0200

    'WalidacjaDanychOrazOdwolania'

--- calc.py
+++ calc.py
@@ -1,4 +1,44 @@
-import math #do logarytmu
+import re
+import math
+#re do dokumentacji panowie poczytajcie cos i napiszcie o tym imporcie, mozecie opisac jak dziala ten patern ponziej  
+#math do logarytmu o dowolnej podstawie
+
+#potrzebny do walidacji danych liczbowych
+pattern = re.compile(r'(^[-]?\d+\.?\d*[+-]\d+\.?\d*j$|^[-]?\d+\.?\d*j?$)')
+
+
+#testy to powyzszego regex
+
+# test = '''
+# -2
+# 2-2j
+# -2-2j
+# -2.2
+# -2.2-2.2j
+# -0
+# 2
+# 2+
+# +2
+# 2j
+# 222j
+# 222
+# 2.222
+# 2.222j
+# 2+2j
+# 2.2+2.2j
+# 22+22
+# j
+# asdas
+# sad+sad
+# '''
+# raw = test.split('\n')
+# matches = pattern.match(test)
+# for match in raw:
+#    if pattern.match(match):
+#        print(match)
+
+
+# ##### WŁAŚCIWY KOD
 
 #kolejka potrzebna do przechowywania ostatnich 10 dzialan
 class Queue(object):
@@ -58,6 +98,24 @@ def calc(operator, arg1, arg2):
     return current_operation(arg1, arg2)
 
 
+######Funkcje do walidacji danych wejsciowych
+def isValid(text, expression):
+    return True if text not in  expression else False
+
+def isValidDigit(text, pattern):
+    variable = input(text)
+    while not (re.search(pattern, variable)):
+        print('Bledne dane! Format to X, Yj lub X+Yj!')
+        variable = input(text)
+    return variable
+
+def validatioInputString(input_string, expression):
+    variable = input(input_string)
+    while isValid(variable, expression):
+        print('Bledne dane! ')
+        variable = input(input_string)
+    return variable
+
 
 operators = {'1' : add, '2' : sub,'3' : mul, '4' : div, '5' : pot, '6' : nth_root, '7' : logarytm}
 
@@ -72,10 +130,30 @@ if __name__ == '__main__':
         '------------------------------\n')
         while True:
             #Przyjmowanie danych od uzytkownika
-            operator = input('Wybierz dzialanie +(1) -(2) *(3) /(4) ^(5) \u221A(6) log(7): ')
+            operator = validatioInputString('Wybierz dzialanie +(1) -(2) *(3) /(4) ^(5) \u221A(6) log(7): ', operators.keys())
+            ans1, ans2  = None, None
+            #wybor wynikow do uzywanych argumentow
+            if store.size_of() > 0:
+                prev = validatioInputString('Czy chcesz użyc poprzecnich wynikow? Y/N ', ['Y', 'N'])
+                if prev == 'Y':
+                    for idx, sol in enumerate(store):
+                        print(f'{idx+1}.', end = " ")
+                        print(sol)
+                    ans1 = int(validatioInputString('Podaj numer wyniku do 1 argumentu lub 0 jeżeli nie chcesz nic wpisywać: ', [str(x) for x in range(store.size_of() + 1)]))
+                    ans2 = int(validatioInputString('Podaj numer wyniku do 2 argumentu lub 0 jeżeli nie chcesz nic wpisywać: ', [str(x) for x in range(store.size_of() + 1)]))
+                    ans1 = store.operations[ans1 - 1] if ans1 != 0 else None
+                    ans2 = store.operations[ans2 - 1] if ans2 != 0 else None
             #wyprowadzenie argumentow         
-            arg1 = input('Podaj 1 arg: ')  
-            arg2 = input('Podaj 2 arg: ')
+            if ans1 == None:
+                arg1 = isValidDigit('Podaj 1 arg: ', pattern)
+                arg1 = complex(arg1)
+            else: 
+                arg1 = ans1
+            if ans2 == None:    
+                arg2 = isValidDigit('Podaj 2 arg: ', pattern)
+                arg2 = complex(arg2)
+            else: 
+                arg2 = ans2
 
             #dodanie do kolejki wyniku
             solution = calc(operator, arg1, arg2)
@@ -88,9 +166,9 @@ if __name__ == '__main__':
             #podanie wyniku i pytanie o czyszczenie pamieci
             print('Wynik:', end = " ")
             print(solution)
-            read = input('Czy chcesz zobaczyc historie dzialan? Y/N ')
+            read = validatioInputString('Czy chcesz zobaczyc historie dzialan? Y/N ', ['Y', 'N'])
             if read == 'Y':
-                clear = input('Czy chcesz wyczyscic CALA pamiec? Y/N ')
+                clear = validatioInputString('Czy chcesz wyczyscic CALA pamiec? Y/N ', ['Y', 'N'])
                 if clear == 'Y':
                     store.operations = []
                     print('Pamiec wyczyszczona! ')
