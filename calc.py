4fe52ec931308771606b5324ea47a45bc256ffd2
Jakub <jakub.tom16@gmail.com>
2022-06-07 21:43:22 +0200

    cały plik

--- calc.py
+++ calc.py
@@ -1,64 +1,65 @@
 import re
 import math
-#re do dokumentacji panowie poczytajcie cos i napiszcie o tym imporcie, mozecie opisac jak dziala ten patern ponziej  
-#math do logarytmu o dowolnej podstawie
 
-#potrzebny do walidacji danych liczbowych
+# re do dokumentacji panowie poczytajcie cos i napiszcie o tym imporcie, mozecie opisac jak dziala ten patern ponziej
+# math do logarytmu o dowolnej podstawie
+
+
+# potrzebny do walidacji danych liczbowych
 pattern = re.compile(r'(^[-]?\d+\.?\d*[+-]\d+\.?\d*j$|^[-]?\d+\.?\d*j?$)')
 
+# testy to powyzszego regex
 
-#testy to powyzszego regex
-
-# test = '''
-# -2
-# 2-2j
-# -2-2j
-# -2.2
-# -2.2-2.2j
-# -0
-# 2
-# 2+
-# +2
-# 2j
-# 222j
-# 222
-# 2.222
-# 2.222j
-# 2+2j
-# 2.2+2.2j
-# 22+22
-# j
-# asdas
-# sad+sad
-# '''
-# raw = test.split('\n')
-# matches = pattern.match(test)
-# for match in raw:
-#    if pattern.match(match):
-#        print(match)
+test = '''
+-2
+2-2j
+-2-2j
+-2.2
+-2.2-2.2j
+-0
+2
+2+
++2
+2j
+222j
+222
+2.222
+2.222j
+2+2j
+2.2+2.2j
+22+22
+j
+asdas
+sad+sad
+'''
+raw = test.split('\n')
+matches = pattern.match(test)
+for match in raw:
+    if pattern.match(match):
+        print(match)
 
 
-# ##### WŁAŚCIWY KOD
+##### WŁAŚCIWY KOD
 
-#kolejka potrzebna do przechowywania ostatnich 10 dzialan
+# kolejka potrzebna do przechowywania ostatnich 10 dzialan
 class Queue(object):
-    #konstruktor inicjalizuje pusta liste
+    # konstruktor inicjalizuje pusta liste
     def __init__(self):
         self.operations = []
 
-    #metoda do wpisywania na ostatnie miejsce kolejki nowa operacje
+    # metoda do wpisywania na ostatnie miejsce kolejki nowa operacje
     def enqueue(self, operation):
         self.operations.insert(0, operation)
-    
-    #Metoda do usuwania pierwszej operacji 
+
+    # Metoda do usuwania pierwszej operacji
     def dequeue(self):
         return self.operations.pop()
 
-    #zwraca dlugosc kolejki
+    # zwraca dlugosc kolejki
     def size_of(self):
         return len(self.operations)
 
-    #umozliwia iterowanie w petli po elementach kolejki
+    # umozliwia iterowanie w petli po elementach kolejki
     def __iter__(self):
         return iter(self.operations)
 
@@ -67,40 +68,40 @@ class Queue(object):
 def add(arg1, arg2):
     return arg1 + arg2
 
+
 def sub(arg1, arg2):
     return arg1 - arg2
+
 
 def mul(arg1, arg2):
     return arg1 * arg2
 
+
 def div(arg1, arg2):
-    if arg2 != 0:
-        return arg1 / arg2
-    else: 
-        print("Nie dziel przez 0!!!")  
+    return arg1 / arg2
+
 
 def pot(podstawa, wykladnik):
-    return podstawa**wykladnik
+    return podstawa ** wykladnik
 
-def nth_root(num ,n):
-    return num**(1/n)
+
+def nth_root(num, n):
+    return num ** (1 / n)
+
 
 def logarytm(podstawa, arg):
-    b = arg.real
-    a = podstawa.real
-    if (a > 0 and a != 1) and b > 0:
-        return math.log(arg.real, podstawa.real)
-    else:
-        print('Liczby z poza dziedziny logarytmu!')
+    return math.log(arg.real, podstawa.real)
 
-def calc(operator, arg1, arg2):    
-    current_operation = operators[operator]
+
+def calc(operator, arg1, arg2):
+    current_operation = operators[operator][0]
     return current_operation(arg1, arg2)
 
 
 ######Funkcje do walidacji danych wejsciowych
 def isValid(text, expression):
-    return True if text not in  expression else False
+    return True if text not in expression else False
+
 
 def isValidDigit(text, pattern):
     variable = input(text)
@@ -109,6 +110,7 @@ def isValidDigit(text, pattern):
         variable = input(text)
     return variable
 
+
 def validatioInputString(input_string, expression):
     variable = input(input_string)
     while isValid(variable, expression):
@@ -116,7 +118,8 @@ def validatioInputString(input_string, expression):
         variable = input(input_string)
     return variable
 
-###FUNCKJE do formatowania wypisywania liczb 
+
+###FUNCKJE do formatowania wypisywania liczb
 
 def printForm(sol):
     if sol.imag == 0:
@@ -126,45 +129,66 @@ def printForm(sol):
     else:
         print(f'{sol:.4f}')
 
-operators = {'1' : add, '2' : sub,'3' : mul, '4' : div, '5' : pot, '6' : nth_root, '7' : logarytm}
+
+#### PETLA GLOWNA
 
 if __name__ == '__main__':
     try:
-        operators = {'1' : add, '2' : sub,'3' : mul, '4' : div, '5' : pot, '6' : nth_root, '7' : logarytm}
-        #str_operators = {'1' : '+', '2' : '-','3' : '*', '4' : '/', '5' : '^', '6' : '^1/'}
+        operators = {'1': [add, "Arg1 + Arg2"], '2': [sub, "Arg1 - Arg2"], '3': [mul, "Arg1 * Arg2"],
+                     '4': [div, "Arg1 / Arg2"], '5': [pot, "Arg1 ^ Arg2"],
+                     '6': [nth_root, "Arg1 liczba pierwiastkowana, Arg2 st. pierwiastka"], '7': [logarytm,
+                                                                                                 "Arg1 to podstawa logarytmu, Arg2 to liczba logarytmowana\nLogarytm jest liczony jedynie z liczb RZECZYWISTYCH!!!"]}
+        # str_operators = {'1' : '+', '2' : '-','3' : '*', '4' : '/', '5' : '^', '6' : '^1/'}
         store = Queue()
-        print('\n------------------------------\n'+
-        'Witaj w poteznym kalkulatorze TS, zostaniesz poproszony o wybor dzialnia ' +
-        'oraz podanie argumrtow dzialnia.\nLiczby zespolone nalezy wpisywac w formacie X+Yj!\n'+
-        '------------------------------\n')
+        print('\n------------------------------\n' +
+              'Witaj w poteznym kalkulatorze TS, zostaniesz poproszony o wybor dzialnia ' +
+              'oraz podanie argumrtow dzialnia.\nLiczby zespolone nalezy wpisywac w formacie X+Yj!\n' +
+              '------------------------------\n')
         while True:
-            #Przyjmowanie danych od uzytkownika
-            operator = validatioInputString('Wybierz dzialanie +(1) -(2) *(3) /(4) ^(5) \u221A(6) log(7): ', operators.keys())
-            ans1, ans2  = None, None
-            #wybor wynikow do uzywanych argumentow
+            # Przyjmowanie danych od uzytkownika
+            operator = validatioInputString('Wybierz dzialanie +(1) -(2) *(3) /(4) ^(5) \u221A(6) log(7): ',
+                                            operators.keys())
+            print()
+            print(operators[operator][1])
+            print()
+            ans1, ans2 = None, None
+            # wybor wynikow do uzywanych argumentow
             if store.size_of() > 0:
                 prev = validatioInputString('Czy chcesz użyc poprzecnich wynikow? Y/N ', ['Y', 'N'])
                 if prev == 'Y':
                     for idx, sol in enumerate(store):
-                        print(f'{idx+1}.', end = " ")
-                        print(sol)
-                    ans1 = int(validatioInputString('Podaj numer wyniku do 1 argumentu lub 0 jeżeli nie chcesz nic wpisywać: ', [str(x) for x in range(store.size_of() + 1)]))
-                    ans2 = int(validatioInputString('Podaj numer wyniku do 2 argumentu lub 0 jeżeli nie chcesz nic wpisywać: ', [str(x) for x in range(store.size_of() + 1)]))
+                        print(f'{idx + 1}.', end=" ")
+                        printForm(sol)
+                    ans1 = int(
+                        validatioInputString('Podaj numer wyniku do 1 argumentu lub 0 jeżeli nie chcesz nic wpisywać: ',
+                                             [str(x) for x in range(store.size_of() + 1)]))
+                    ans2 = int(
+                        validatioInputString('Podaj numer wyniku do 2 argumentu lub 0 jeżeli nie chcesz nic wpisywać: ',
+                                             [str(x) for x in range(store.size_of() + 1)]))
                     ans1 = store.operations[ans1 - 1] if ans1 != 0 else None
                     ans2 = store.operations[ans2 - 1] if ans2 != 0 else None
-            #wyprowadzenie argumentow         
+            # wyprowadzenie argumentow
             if ans1 == None:
-                arg1 = isValidDigit('Podaj 1 arg: ', pattern)
-                arg1 = complex(arg1)
-            else: 
+                arg1 = complex(isValidDigit('Podaj 1 arg: ', pattern))
+            else:
                 arg1 = ans1
-            if ans2 == None:    
-                arg2 = isValidDigit('Podaj 2 arg: ', pattern)
-                arg2 = complex(arg2)
-            else: 
+            if ans2 == None:
+                arg2 = complex(isValidDigit('Podaj 2 arg: ', pattern))
+            else:
                 arg2 = ans2
+            # blok sprawdzający warunki pierwiastka, pierwiastka n stopnia
+            while not arg2 and operator == '4' or operator == '6':
+                print("Nie dziel przez 0!!!") if operator == '4' else print("Pierwiastek nie może być stopnia 0!!!")
+                arg2 = complex(isValidDigit('Podaj 2 arg: ', pattern))
+            while not (arg1.real > 0 and arg1.real != 1) and arg2.real <= 0:
+                if not (arg1.real > 0 and arg1.real != 1):
+                    print("Podstawa musi być dodatnia i różna od 1!!!")
+                    arg1 = complex(isValidDigit('Podaj 1 arg: ', pattern))
+                else:
+                    print("Liczba logarytmowana musi być dodatnia!!!")
+                    arg2 = complex(isValidDigit('Podaj 1 arg: ', pattern))
 
-            #dodanie do kolejki wyniku
+            # dodanie do kolejki wyniku
             solution = calc(operator, arg1, arg2)
             if store.size_of() == 10:
                 store.dequeue()
@@ -172,9 +196,9 @@ if __name__ == '__main__':
             else:
                 store.enqueue(solution)
 
-            #podanie wyniku i pytanie o czyszczenie pamieci
-            print('Wynik:', end = " ")
-            print(solution)
+            # podanie wyniku i pytanie o czyszczenie pamieci
+            print('Wynik:', end=" ")
+            printForm(solution)
             read = validatioInputString('Czy chcesz zobaczyc historie dzialan? Y/N ', ['Y', 'N'])
             if read == 'Y':
                 clear = validatioInputString('Czy chcesz wyczyscic CALA pamiec? Y/N ', ['Y', 'N'])
@@ -184,8 +208,8 @@ if __name__ == '__main__':
                 elif clear == 'N':
                     print('Ostatniew dzialania to... ')
                     for idx, sol in enumerate(store):
-                        print(f'{idx+1}.', end = " ")
-                        print(sol)
-                        
+                        print(f'{idx + 1}.', end=" ")
+                        printForm(sol)
+
     except KeyboardInterrupt:
         print("Wyjscie z programu")
